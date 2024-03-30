187dff40bef3e37a992836b3341b4a94c0fc590a
LonelyAvon <92423766+LonelyAvon@users.noreply.github.com>
2024-03-30 11:16:07 +0700

    Initial commit

---
+++ Entities/Discipline.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace _Cytrikov_Var3.Entities
+{
+	public class Discipline
+	{
+
+		public int IdDiscipline { get; set; }
+
+		public string? Title { get; set; }
+
+	}
+}
+
---
+++ MainWindow.axaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using _Cytrikov_Var3.Entities;
+using Avalonia.Controls;
+
+namespace _21._102_Cytrikov_Var3;
+
+public partial class MainWindow : Window
+{
+    public ObservableCollection<Discipline> _discipline = new ObservableCollection<Discipline>
+    {
+        new Discipline{ IdDiscipline=1, Title = "Математика"},
+        new Discipline{ IdDiscipline=2, Title = "Русский язык"},
+        new Discipline{ IdDiscipline=3, Title = "Музыка"},
+        new Discipline{ IdDiscipline=4, Title = "Труд"},
+        new Discipline{ IdDiscipline=5, Title = "Физика"},
+    };
+    public ObservableCollection<string> a = new ObservableCollection<string>
+    {
+        "по возрастанию",
+        "по убыванию"
+    };
+    public MainWindow()
+    {
+        InitializeComponent();
+        Table.ItemsSource = _discipline;
+        Box.ItemsSource = a;
+    }
+}
